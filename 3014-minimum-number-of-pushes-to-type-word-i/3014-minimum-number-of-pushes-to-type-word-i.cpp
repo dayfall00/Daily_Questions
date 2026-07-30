a62a50a17eed12c78358f18cb9dad9fac6815567
Aditya Bhardwaj <motordeath05@gmail.com>
2026-07-30 11:59:03 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int s= word.size();
-        int cost=0;
-        int r,q;
-        q=s/8;
-        r=s%8;
-        int i=1;
-        cost=(q*(q+1))*4+(q+1)*r;
+        // int cost=0;
+        // int r,q;
+        // q=s/8;
+        // r=s%8;
+        // int i=1;
+        // cost=(q*(q+1))*4+(q+1)*r;
         // for(;i<=q;i++)
         // {
         //     cost+=i*8;
         // }
         // cost+=(q+1)*r;
-        return cost;
+        return  ((s / 8) * ((s / 8) + 1)) * 4 + ((s / 8) + 1) * (s % 8);
 
     }
 };
