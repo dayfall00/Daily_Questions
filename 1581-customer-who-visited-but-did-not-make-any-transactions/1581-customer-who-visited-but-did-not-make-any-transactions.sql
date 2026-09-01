c9967655255f084c8fee52c9fe86ec0b3d9d8a5d
Aditya Bhardwaj <motordeath05@gmail.com>
2026-09-01 18:54:02 +0530

    Time: 2903 ms (5%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -2,12 +2,20 @@
 -- 1) find those visit ids those who have made transactions
 -- 2) get those customers who havent made any transactions
 -- 3) group by customer_id then count        
+-- select customer_id
+--     , count(visit_id) as count_no_trans
+--     from(    select *
+--         from visits
+--         where visit_id not in   (
+--                     select  distinct visit_id
+--                     from transactions   )
+--     )as customer_no_trans
+-- group by customer_id
+
 select customer_id
     , count(visit_id) as count_no_trans
-    from(    select *
-        from visits
-        where visit_id not in   (
+    from visits 
+    where visit_id not in (
                     select  distinct visit_id
                     from transactions   )
-    )as customer_no_trans
 group by customer_id
