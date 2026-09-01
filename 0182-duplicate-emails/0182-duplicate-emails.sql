155d6ef8601218cbf06ebbddf576ee60697aac1b
Aditya Bhardwaj <motordeath05@gmail.com>
2026-09-01 19:17:32 +0530

    Time: 571 ms (9.57%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,14 @@
 # Write your MySQL query statement below
-select email from 
-Person
-group by email
-having count(id)>1
+-- select email from 
+-- Person
+-- group by email
+-- having count(id)>1
+
+
+select email
+from(
+    select email, count(id) as cnt
+    from Person
+    group by email
+) as fx
+where cnt>1
