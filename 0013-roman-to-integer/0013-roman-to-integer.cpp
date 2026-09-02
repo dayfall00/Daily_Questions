d8ed913ac6131cc7b707c1d89740aa8249226cf1
Aditya Bhardwaj <motordeath05@gmail.com>
2026-09-02 10:13:13 +0530

    Time: 0 ms (100%), Space: 10.1 MB (77.52%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     private:
     int value(char ch1){
         int I             = 1;
@@ -65,5 +65,41 @@ public:
 
 
 
+    }
+};
+
+// CLEANER CODE
+
+
+class Solution {
+private:
+    int value(char ch) {
+        switch(ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return -1;
+    }
+
+public:
+    int romanToInt(string s) {
+        int total = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            int current = value(s[i]);
+
+            if(i + 1 < s.length() && current < value(s[i + 1]))
+                total -= current;
+            else
+                total += current;
+        }
+
+        return total;
     }
 };
