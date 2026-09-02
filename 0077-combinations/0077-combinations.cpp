1b6137edab306eea01198a58cd4900ed93da9655
Aditya Bhardwaj <motordeath05@gmail.com>
2026-09-02 19:02:46 +0530

    Time: 67 ms (55.03%), Space: 110.1 MB (18.4%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> combinations;
@@ -29,5 +29,32 @@ private:
         partialSubset.push_back(i);
         dfs(n, i + 1, k, partialSubset, combinations);
         partialSubset.pop_back();
+    }
+};
+
+// backtracking
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> combinations;
+        vector<int> partial;
+        dfs(n,k,partial, combinations);
+        return combinations;
+
+    }
+private:
+    void dfs( int n, int k , vector<int>& partial, vector<vector<int>>& combinations){
+
+        if(k == 0) combinations.push_back(partial);
+        else{
+            // without n
+            if(n>k) dfs(n-1,k,partial, combinations);
+
+            // with n
+
+            partial.push_back(n);
+            dfs(n-1,k-1,partial, combinations);
+            partial.pop_back();
+        }
     }
 };
