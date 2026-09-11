5d0673fed7923215dc7287f4ec91d3daa6409a10
Aditya Bhardwaj <motordeath05@gmail.com>
2026-09-11 18:59:05 +0530

    Time: 379 ms (36.36%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,15 +1,10 @@
 # Write your MySQL query statement below
--- 1) find no of students in each class
--- select class from 
---     (   select distinct class, 
---         count(student) as cnt 
---         from courses 
---         group by class) 
--- as fx
--- where cnt>=5
-
-
-select class 
-from courses
-group by class
-having count(student) >=5
+with students_per_class as
+(
+    select class,
+    count(student) as std_count
+    from Courses
+    group by class
+)
+select class from students_per_class
+where std_count>=5;
